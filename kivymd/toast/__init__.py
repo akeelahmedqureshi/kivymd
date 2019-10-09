f6ce75e63025b3835cf37000be7aed6e3b7b2f26
surbhi <surbhi.s@cisinlabs.com>
2019-10-09 19:59:10 +0530

    testing toast error for android

--- kivymd/toast/__init__.py
+++ kivymd/toast/__init__.py
@@ -13,11 +13,13 @@ Copyright © 2010-2018 HeaTTheatR
 что и фреймворк Kivy.
 
 '''
-
+print("inside toast init method.......")
 from kivy import platform
 
 
 if platform == 'android': 
+	print("befor toast module import........")
     from . androidtoast import toast
+    print("after toast module import........")
 else:
     from . kivytoast import toast
--- kivymd/toast/androidtoast/androidtoast.py
+++ kivymd/toast/androidtoast/androidtoast.py
@@ -12,19 +12,31 @@ Copyright © 2010-2018 HeaTTheatR
 что и фреймворк Kivy.
 
 '''
-
+print("inside androidtoast file....................")
 from kivy.logger import Logger
+print("check error.......................................1")
 from jnius import autoclass, PythonJavaClass, java_method, cast
+print("check error.......................................2")
 from android import activity
+print("check error.......................................3")
 from android.runnable import run_on_ui_thread
+print("check error.......................................4")
 
 Toast = autoclass('android.widget.Toast')
-context = autoclass('org.renpy.android.PythonActivity').mActivity    
+print("check error.......................................5")
+context = autoclass('org.renpy.android.PythonActivity').mActivity
+print("check error.......................................6")
 
 @run_on_ui_thread
 def toast(text, length_long=False):
+	print("check error.......................................7")
     duration = Toast.LENGTH_LONG if length_long else Toast.LENGTH_SHORT
+    print("check error.......................................8")
     String = autoclass('java.lang.String')
+    print("check error.......................................9")
     c = cast('java.lang.CharSequence', String(text))
+    print("check error.......................................10")
     t = Toast.makeText(context, c, duration)
+    print("check error.......................................11")
     t.show()
+    print("check error.......................................12")
